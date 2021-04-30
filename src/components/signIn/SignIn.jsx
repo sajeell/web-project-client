e31ad8b6713fa61c639dc40204e439bf927b5e30
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-04-30 15:41:00 +0500

    :art: Complete Frontend Of SignIn & SignUp Pages

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,9 +1,37 @@
+
 import "./SignIn.scss"
+
+import FormBG from "../../static/images/form-bg.png"
 
 export default function SignIn() {
     return (
         <div className="signin-wrapper">
-            <h1>Sign In</h1>
+            <div className="signin-bg-image">
+                <img src={FormBG} alt="Form background" />
+            </div>
+            <div className="signin-form">
+                <form>
+                    <div className="signin-row-1">
+                        <span>Sign In</span>
+                    </div>
+                    <div className="signin-row-2">
+                        <input type="email" name="email" id="email" placeholder="Email Address" />
+                    </div>
+                    <div className="signin-row-3">
+                        <input type="password" name="password" id="password" placeholder="Password" />
+                    </div>
+
+                    <div className="signin-row-4">
+                        <input type="submit" name="submit-btn" id="submit-btn" value="Sign In" />
+                    </div>
+
+                    <div className="signin-row-5">
+                        <span className="signin-row-5-item">Don't have an account yet?</span>
+                        <span className="signin-row-5-item">Sign Up</span>
+                        <span className="signin-row-5-item">here</span>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
